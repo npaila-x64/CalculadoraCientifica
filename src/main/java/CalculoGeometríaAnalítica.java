9a1b489c247a9a55faafc5d7cb657e5a6c6924aa
N. Paila <n.paila01@ufromail.cl>
2022-09-13 11:26:31 -0300

    agrega clase con calculos geométrico analíticos y sus pruebas unitarias respectivas

---
+++ src/main/java/CalculoGeometríaAnalítica.java
@@ -0,0 +1,47 @@
+public class CalculoGeometríaAnalítica {
+
+    public static double calcularPerimetroCuadrilatero(double l1, double l2, double l3, double l4) {
+        return l1+l2+l3+l4;
+    }
+
+    public static double calcularAreaCuadrilatero(double l1, double l2) {
+        return l1*l2;
+    }
+
+    public static double calcularPerimetroCirculo(double radio) {
+        return 2.0*Math.PI*radio;
+    }
+
+    public static double calcularAreaCirculo(double radio) {
+        return Math.PI*Math.pow(radio, 2.0);
+    }
+
+    public static double calcularAreaCubo(double lado) {
+        return 6.0*Math.pow(lado, 2.0);
+    }
+
+    public static double calcularAreaEsfera(double radio) {
+        return 4.0*Math.PI*Math.pow(radio, 2.0);
+    }
+
+    public static double calcularAreaCono(double radio, double altura) {
+        return Math.PI*radio*Math.pow(
+                Math.pow(radio, 2.0)+Math.pow(altura, 2.0), 0.5)+Math.PI*Math.pow(radio, 2.0);
+    }
+
+    public static double calcularPerimetroCubo(double lado) {
+        return 12.0*lado;
+    }
+
+    public static double calcularVolumenEsfera(double radio) {
+        return (4.0/3.0)*Math.PI*Math.pow(radio, 3.0);
+    }
+
+    public static double calcularVolumenCubo(double lado) {
+        return Math.pow(lado, 3.0);
+    }
+
+    public static double calcularVolumenCono(double radio, double altura) {
+        return Math.PI*Math.pow(radio, 2.0)*(altura/3.0);
+    }
+}
---
+++ src/test/java/CalculoGeometríaAnalíticaTest.java
@@ -0,0 +1,72 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculoGeometríaAnalíticaTest {
+
+    @Test
+    void verificaCalcularPerimetroCuadrilateroTest() {
+        double perimetro = CalculoGeometríaAnalítica.calcularPerimetroCuadrilatero(2, 1, 5.5, 1.6);
+        assertEquals(10.1, perimetro, 1e-15);
+    }
+
+    @Test
+    void verificaCalcularAreaCuadrilateroTest() {
+        double area = CalculoGeometríaAnalítica.calcularAreaCuadrilatero(2.2, 4.5);
+        assertEquals(9.9, area, 1e-15);
+    }
+
+    @Test
+    void verificaCalcularPerimetroCirculoTest() {
+        double perimetro = CalculoGeometríaAnalítica.calcularPerimetroCirculo(4.2);
+        assertEquals(26.389378290154262, perimetro, 1e-15);
+    }
+
+    @Test
+    void verificaCalcularAreaCirculoTest() {
+        double perimetro = CalculoGeometríaAnalítica.calcularAreaCirculo(4.2);
+        assertEquals(55.41769440932395, perimetro, 1e-15);
+    }
+
+    @Test
+    void verificaCalcularAreaCuboTest() {
+        double area = CalculoGeometríaAnalítica.calcularAreaCubo(5);
+        assertEquals(150, area, 1e-15);
+    }
+
+    @Test
+    void verificaCalcularPerimetroCuboTest() {
+        double area = CalculoGeometríaAnalítica.calcularPerimetroCubo(2);
+        assertEquals(24, area, 1e-15);
+    }
+
+    @Test
+    void verificaCalcularAreaEsferaTest() {
+        double area = CalculoGeometríaAnalítica.calcularAreaEsfera(4);
+        assertEquals(201.06192982974676, area, 1e-15);
+    }
+
+    @Test
+    void verificaCalcularAreaConoTest() {
+        double area = CalculoGeometríaAnalítica.calcularAreaCono(2, 1);
+        assertEquals(26.616000076440628, area, 1e-15);
+    }
+
+    @Test
+    void verificaCalcularVolumenEsferaTest() {
+        double volumen = CalculoGeometríaAnalítica.calcularVolumenEsfera(3);
+        assertEquals(113.09733552923254, volumen, 1e-15);
+    }
+
+    @Test
+    void verificaCalcularVolumenCuboTest() {
+        double volumen = CalculoGeometríaAnalítica.calcularVolumenCubo(2.5);
+        assertEquals(15.625, volumen, 1e-15);
+    }
+
+    @Test
+    void verificaCalcularVolumenConoTest() {
+        double volumen = CalculoGeometríaAnalítica.calcularVolumenCono(2, 1);
+        assertEquals(4.1887902047863905, volumen, 1e-15);
+    }
+}
