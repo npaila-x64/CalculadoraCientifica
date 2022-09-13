447e1ac91bbf71b0486c40fce4402b6aa66369d6
Jorge <jorge.quidel312@gmail.com>
2022-09-13 10:15:33 -0300

    crea clase para operaciones aritméticas básicas

---
+++ src/main/java/OperacionesBasicas.java
@@ -0,0 +1,27 @@
+public class OperacionesBasicas {
+    public static void main(String[] args) {
+        System.out.println(dividir(10,0));
+    }
+    public static double sumar(double num1, double num2){
+        return num1+num2;
+    }
+    public static double restar(double num1, double num2){
+        return num1-num2;
+    }
+    public static double multiplicar(double num1, double num2){
+        return num1*num2;
+    }
+    public static double dividir(double dividendo, double divisor){
+        return dividendo/divisor;
+    }
+    public static double numeroMenor(double num1, double num2){
+        return Math.min(num1, num2);
+    }
+    public static double elevarNumero(double base, double exponente){
+        return Math.pow(base, exponente);
+    }
+    public static double calcularPorcentaje(double num1, double num2){
+        double porcentaje = num2/100;
+        return num1*porcentaje;
+    }
+}
