4f4d5f0669d99aacb8e1c7245d02191cfc09d869
Jorge <jorge.quidel312@gmail.com>
2022-09-15 21:13:18 -0300

    agrega condiciones para casos indefinidos

--- src/main/java/OperacionesBasicas.java
+++ src/main/java/OperacionesBasicas.java
@@ -12,12 +12,18 @@ public class OperacionesBasicas {
         return num1*num2;
     }
     public static double dividir(double dividendo, double divisor){
+        if(divisor == 0){
+            return Double.NaN;
+        }
         return dividendo/divisor;
     }
     public static double numeroMenor(double num1, double num2){
         return Math.min(num1, num2);
     }
     public static double elevarNumero(double base, double exponente){
+        if(base == 0 && exponente == 0){
+            return Double.NaN;
+        }
         return Math.pow(base, exponente);
     }
     public static double calcularPorcentaje(double num1, double num2){
