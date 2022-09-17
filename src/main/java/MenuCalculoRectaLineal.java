9ee391ab2a5288759da3a69a617627a87053b41d
N. Paila <n.paila01@ufromail.cl>
2022-09-16 23:08:39 -0300

    agrega estructura funcional a métodos de cálculo de recta

--- src/main/java/MenuCalculoRectaLineal.java
+++ src/main/java/MenuCalculoRectaLineal.java
@@ -1,26 +1,26 @@
 public class MenuCalculoRectaLineal {
 
     public static double calcularPendiente(double x1, double y1, double x2, double y2) {
-
-
-        return 0.0;
+        double pendiente = (y1 - y2)/(x1 - x2);
+        return pendiente;
     }
 
     public static String obtenerEcuacion(double x1, double y1, double x2, double y2) {
-
-
-        return "";
+        double pendiente = calcularPendiente(x1, y1, x2, y2);
+        double intercepcion = -pendiente*x1 + y1;
+        return String.format("y=%5.15fx+%5.15f", pendiente, intercepcion);
     }
 
     public static double calcularInterseccionEjeX(double x1, double y1, double x2, double y2) {
-
-
-        return 0.0;
+        double pendiente = calcularPendiente(x1, y1, x2, y2);
+        double intercepcion = -pendiente*x1 + y1;
+        double interseccionX = -intercepcion / pendiente;
+        return interseccionX;
     }
 
     public static double calcularInterseccionEjeY(double x1, double y1, double x2, double y2) {
-
-
-        return 0.0;
+        double pendiente = calcularPendiente(x1, y1, x2, y2);
+        double intercepcion = -pendiente*x1 + y1;
+        return intercepcion;
     }
 }
--- src/test/java/MenuCalculoRectaLinealTest.java
+++ src/test/java/MenuCalculoRectaLinealTest.java
@@ -8,14 +8,14 @@ class MenuCalculoRectaLinealTest {
     void calcularPendienteTest() {
         double x1 = 3; double y1 = 3; double x2 = 1; double y2 = 1;
         double pendienteCalculada = MenuCalculoRectaLineal.calcularPendiente(x1, y1, x2, y2);
-        double pendienteReal = 2;//(y1 - y2)/(x1 - x2);
+        double pendienteReal = 1;
         assertEquals(pendienteReal, pendienteCalculada);
     }
 
     @Test
     void obtenerEcuacionTest() {
         double x1 = 1; double y1 = 2; double x2 = -2; double y2 = 3;
-        String ecuacionReal = "y=-0.33333333x-2.33333333";
+        String ecuacionReal = "y=-0.333333333333333x+2.333333333333334";
         String ecuacionObtenida = MenuCalculoRectaLineal.obtenerEcuacion(x1, y1, x2, y2);
         assertEquals(ecuacionReal, ecuacionObtenida);
     }
@@ -25,7 +25,7 @@ class MenuCalculoRectaLinealTest {
         double x1 = 1; double y1 = 2; double x2 = -2; double y2 = 3;
         double interseccionReal = 7;
         double interseccionObtenida = MenuCalculoRectaLineal.calcularInterseccionEjeX(x1, y1, x2, y2);
-        assertEquals(interseccionReal, interseccionObtenida, 1e-6);
+        assertEquals(interseccionReal, interseccionObtenida, 1e-15);
     }
 
     @Test
