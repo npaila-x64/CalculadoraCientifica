0876b40f55f024555eb27064d8d756b7b125be6b
N. Paila <n.paila01@ufromail.cl>
2022-09-16 22:50:47 -0300

    crea métodos vacíos para clase recta lineal

--- src/main/java/MenuCalculoRectaLineal.java
+++ src/main/java/MenuCalculoRectaLineal.java
@@ -1,3 +1,26 @@
 public class MenuCalculoRectaLineal {
 
+    public static double calcularPendiente(double x1, double y1, double x2, double y2) {
+
+
+        return 0.0;
+    }
+
+    public static String obtenerEcuacion(double x1, double y1, double x2, double y2) {
+
+
+        return "";
+    }
+
+    public static double calcularInterseccionEjeX(double x1, double y1, double x2, double y2) {
+
+
+        return 0.0;
+    }
+
+    public static double calcularInterseccionEjeY(double x1, double y1, double x2, double y2) {
+
+
+        return 0.0;
+    }
 }
